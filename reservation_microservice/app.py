3de03efcc1c27a9ca91c24e613a90d2020751e5f
Sbrasbez19 <mariosaracino19@gmail.com>
2020-11-23 12:48:49 +0100

    Added reservatopm system operator side. Updated endpoints. Minor fixes.

--- reservation_microservice/app.py
+++ reservation_microservice/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import connexion, logging
-from reservation_microservice.database import db, Reservation
+from reservation_microservice.database import db, Reservation, ReservationState
 from datetime import datetime, time
 
 def create_app(dbfile='sqlite:///reservations_service.db'):
@@ -15,14 +15,32 @@ def create_app(dbfile='sqlite:///reservations_service.db'):
     application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(application)
-    #db.drop_all(app=application)
+    db.drop_all(app=application)
     db.create_all(app=application)
 
-    with application.app_context():
-        if (db.session.query(Reservation).first() is None):
-            reservation = Reservation(user_id=1, restaurant_id=1, seats=4, table_no=2, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
-            db.session.add(reservation)
-            db.session.commit()
+    #with application.app_context():
+        #if (db.session.query(Reservation).first() is None):
+           # reservation = Reservation(user_id=1, restaurant_id=1, seats=4, table_no=2, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+           # db.session.add(reservation)
+           # db.session.commit()
+        #reservation = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=1, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+        #reservation2 = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=2, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+        #reservation3 = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=3, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+        #reservation4 = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=4, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+        #reservation5 = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=5, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+        #reservation6 = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=6, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+        #reservation7 = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=7, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+        #reservation8 = Reservation(user_id=1, restaurant_id=999, seats=4, table_no=8, reservation_time=datetime.combine(datetime.now().date(), time(hour=21, minute=30)))
+
+        #db.session.add(reservation)
+        #db.session.add(reservation2)
+        #db.session.add(reservation3)
+        #db.session.add(reservation4)
+        #db.session.add(reservation5)
+        #db.session.add(reservation6)
+        #db.session.add(reservation7)
+        #db.session.add(reservation8)
+        #db.session.commit()
 
 
     return application
--- reservation_microservice/restaurant_reservations.py
+++ reservation_microservice/restaurant_reservations.py
@@ -1,27 +1,106 @@
-from reservation_microservice.database import db, Reservation
+from reservation_microservice.database import db, Reservation, ReservationState
 from flask import request, jsonify
 from sqlalchemy.exc import DatabaseError
-from datetime import datetime
+from datetime import datetime, date
+
 
 def get_restaurant_reservations(restaurant_id: int):
-    try:
-        reservations = db.session.query(Reservation).filter_by(restaurant_id = restaurant_id).all()
-    except DatabaseError as exc:
-        return str(exc), 500
-    if (len(reservations) > 0):
-        return [res.to_dict() for res in reservations], 200
-    else:
-        return {'message': 'No reservations associated to the given restaurant_id'}, 404
-    
 
+    print(request.args.to_dict())
+    #request values
+    time_range = request.args.get('range', None)
+    seated = request.args.get('seated', False)
+    page = request.args.get('page', 1)  #default 1
+    page_size = request.args.get('page_size', 6)  #default 6
+
+    reservations = Reservation.query.filter_by(restaurant_id=restaurant_id)
+    #gets only the reservations whose status is SEATED, needed to count the number of seated customers
+    if seated:
+        reservations = reservations.filter_by(status=ReservationState.SEATED)
+        try:
+            res = [r.to_dict() for r in reservations.all()]
+            return {
+                'reservations': res
+            }
+        except DatabaseError as exc:
+            return {'message': str(exc)}, 500
+    #gets the reservation in the specified time range
+    if time_range:
+        if (time_range == 'today'):
+            today = date.today()
+            reservations = reservations.filter(
+                datetime(today.year, today.month, today.day) <
+                Reservation.reservation_time).filter(
+                    Reservation.reservation_time < datetime(
+                        today.year, today.month, today.day + 1))
+        elif (time_range == 'upcoming'):
+            reservations = reservations.filter(
+                Reservation.reservation_time > datetime.now())
+        else:
+            return {
+                'Message': 'time_range can only have value today or upcoming'
+            }, 400
+    #computes the offset based on the page
+    reservations = reservations.offset(
+        (int(page) - 1) * int(page_size)).limit(int(page_size) + 1)
+    try:
+        #returns reservations
+        res = [r.to_dict() for r in reservations.all()]
+        return {
+            'reservations': res
+        } if len(res) > 0 else {
+            'message': 'No Reservations associated to the specified restaurant'
+        }, 404
+    except DatabaseError as exc:
+        return {'message': str(exc)}, 500
+
+
+def update_reservation_status(reservation_id: int):
+    status = request.get_json()['status']
+    #gets the needed reservation
+    try:
+        reservation = Reservation.query.filter_by(id=reservation_id).first()
+        if (reservation is None):
+            return {'message': 'Reservation not found'}, 404
+    except DatabaseError as exc:
+        return {'message': str(exc)}, 500
+    #update the status if status is in [0, 4]
+    try:
+        reservation.status = ReservationState(status)
+    except ValueError:
+        {
+            'message':
+            'status can only be one of the following values: 0 = Declined, 1 = Pending, 2 = Accepted, 3 = Seated, 4 = Done'
+        }, 400
+    #needed to update the entrance/exit time in case status = seated or status = done
+    if (status == 3 or status == 4):
+        try:
+            time = datetime.fromisoformat(request.get_json()['time'])
+        except:
+            return {'message': 'time parameter should be in ISO format'}, 400
+        if (status == 3):
+            reservation.entrance_time = time
+        else:
+            reservation.exit_time = time
+
+    try:
+        db.session.commit()
+        return {'message': 'Reservation updated correctly'}
+    except DatabaseError as exc:
+        return {'message': str(exc)}, 500
 
 def delete_restaurant_reservations(restaurant_id: int):
     try:
-        future_res = db.session.query(Reservation).filter_by(restaurant_id = restaurant_id).filter(Reservation.reservation_time >= datetime.now()).delete()
+        future_res = db.session.query(Reservation).filter_by(
+            restaurant_id=restaurant_id).filter(
+                Reservation.reservation_time >= datetime.now()).delete()
         print(future_res)
         if (future_res == 0):
-            return {'message': 'No future reservations associated to the given restaurant_id'}, 404
-        else: 
+            return {
+                'message':
+                'No future reservations associated to the given restaurant_id'
+            }, 404
+        else:
             db.session.commit()
             return 204
     except DatabaseError as exc:
